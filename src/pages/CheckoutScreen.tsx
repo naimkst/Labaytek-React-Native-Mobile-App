4c2670abe6de609e85fd4d1d56efdaf8cf025442
naimkst <naimkst@gmail.com>
2023-05-27 23:09:22 +0600

    update code

--- src/pages/CheckoutScreen.tsx
+++ src/pages/CheckoutScreen.tsx
@@ -1,12 +1,20 @@
 import {CheckBox} from '@rneui/themed';
 import React from 'react';
 import {Text, View} from 'react-native';
+import {FullWindowOverlay} from 'react-native-screens';
 import Icon from 'react-native-vector-icons/AntDesign';
 
 export const CheckoutScreen = () => {
   const [checked, setChecked] = React.useState(0);
   return (
-    <View className="px-[10px] pt-[10px]">
+    <View
+      className="px-[10px] pt-[10px] mb-[40px]"
+      style={{
+        display: 'flex',
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-evenly',
+      }}>
       <View>
         <Icon name="arrowleft" size={30} color="black" />
       </View>
@@ -62,6 +70,47 @@ export const CheckoutScreen = () => {
             onPress={() => setChecked(3)}
           />
         </View>
+      </View>
+
+      <View className="">
+        <Text className="text-[27px] font-FontBold text-primaryBlack">
+          Payment Summary
+        </Text>
+        <View className="mt-[20px] pb-[20px]">
+          <View className="flex flex-row justify-between items-center mb-3">
+            <Text className="font-FontNormal text-primaryBlack">
+              Basket Total
+            </Text>
+            <Text className="font-FontNormal text-primaryBlack">KD 8.500</Text>
+          </View>
+
+          <View className="flex flex-row justify-between items-center mb-3">
+            <Text className="font-FontNormal text-primaryBlack">
+              Delivery Fee
+            </Text>
+            <Text className="font-FontNormal text-primaryBlack">KD 2.500</Text>
+          </View>
+
+          <View className="flex flex-row justify-between items-center mt-5">
+            <Text className="font-FontBold text-primaryBlack text-[16px]">
+              Total Amount
+            </Text>
+            <Text className="font-FontNormal text-primaryBlack">KD 2.500</Text>
+          </View>
+
+          <View className="mt-[30px]">
+            <Text className="font-FontLight">
+              By placing this order your agree to the Credit Card payment
+            </Text>
+            <Text>terms and conditions</Text>
+          </View>
+        </View>
+      </View>
+
+      <View className="bg-primaryColor flex justify-center items-center py-3 rounded-md ">
+        <Text className="text-white font-FontNormal text-[15px]">
+          Place Order
+        </Text>
       </View>
     </View>
   );
